7a227979a6d3693280c7c295cd01fb471556c033
tassiasP <tassias1pan@gmail.com>
2021-09-28 15:06:13 +0300

    Convert bart output to slot predictions

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import torch
+import re
 from seqeval.metrics import f1_score as seqeval_f1_score
 from seqeval.scheme import IOB2
 from sklearn.metrics import f1_score as sklearn_f1_score
@@ -33,14 +34,41 @@ def intent_metrics(labels, preds):
 def intent_metrics_bart(eval_pred: EvalPrediction):
     logits, labels = eval_pred
     logits = logits[0]
-
     preds = np.argmax(logits, axis=-1)
+    # Replace -100 (CrossEntropy's ignore_index) in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, 1)  # 1 is the pad_token_id
     # For error inspection
     # print(f"{preds=}\n\n {labels=}")
 
     metric = load_metric("accuracy")
 
     return metric.compute(predictions=preds.flatten(), references=labels.flatten())
+
+
+def convert_output_to_slot_preds(prediction_output):
+    # example:
+    #   input = "<s> The playlist owner is none</s>The playlist is grime instrumentals</s> \
+    #   The artist is sabrina salerno</s>The track name is none</s>The music item is none</s> \
+    #   The</s></s></s></s></s></s></s></s></s></s></s></s></s></s></s></s></s></s>"
+    #
+    #   output: {"playlist owner": "none", "playlist": "grime instrumentals", "artist": "sabrina salerno",
+    #             "track name": "none", "music item": none}
+    slot_preds = {}
+    for sent in prediction_output.split('</s>'):
+        match = re.search(r"(?<=The).*", sent)
+        if match:
+            match = match.group(0).strip()
+            print(match)
+            if match != '':
+                slot, _, value = match.partition(' is ')
+                slot, value = slot.strip(), value.strip()
+
+                try:
+                    slot_preds[INVERTED_SLOT_MAPPING[slot]] = value
+                except KeyError:
+                    continue
+
+    return slot_preds
 
 
 # To be used for BART mask filling
@@ -62,7 +90,7 @@ SLOT_MAPPING = {
     "best_rating": "best rating",
     "city": "city",
     "condition_description": "weather condition",
-    "condition_temperature": "condition of temperature",
+    "condition_temperature": "temperature condition",
     "country": "country",
     "cuisine": "cuisine",
     "current_location": "current location",
@@ -96,4 +124,8 @@ SLOT_MAPPING = {
     "timeRange": "time range",
     "track": "track to play",
     "year": "year"
+}
+
+INVERTED_SLOT_MAPPING = {
+    v: k for k, v in SLOT_MAPPING.items()
 }
