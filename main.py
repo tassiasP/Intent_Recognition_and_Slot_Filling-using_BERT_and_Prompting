04a6c9899940cd565ba0b26d36bf80540636f2ac
tassiasP <tassias1pan@gmail.com>
2021-10-13 16:10:30 +0300

    Minor changes

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ import torch.cuda
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Subset
 from omegaconf import OmegaConf
-from transformers import TrainingArguments, Trainer, BartForConditionalGeneration, BartTokenizer, BartConfig, \
+from transformers import BartForConditionalGeneration, BartTokenizer, BartConfig, \
     Seq2SeqTrainingArguments, Seq2SeqTrainer, T5Config, T5Tokenizer, T5ForConditionalGeneration
+from transformers import logging
 
 from joint_dataset import get_dataloader, IntentDataset, SlotDataset
 from models import JointBert
-from train import train, train_seq2seq_model
-from utils import set_seed, intent_metrics_bart, INTENT_MAPPING, convert_bart_output_to_slot_preds, SLOT_MAPPING,\
-    compute_micro_f1
+from train_evaluate import train, train_seq2seq_model
+from utils import set_seed, INTENT_MAPPING, convert_bart_output_to_slot_preds, SLOT_MAPPING, compute_micro_f1
 from reader import Reader
 
 
 def main(run_args, model_config):
     torch.cuda.empty_cache()
     set_seed(run_args.seed)
+    logging.set_verbosity(logging.CRITICAL)
 
     reader = Reader(run_args.dataset)
     reader.construct_intent_and_slot_vocabs(write_to_disk=True)
@@ -65,14 +66,14 @@ def main(run_args, model_config):
 
             tokenizer = BartTokenizer.from_pretrained(checkpoint)
 
-            # checkpoint = './test-bart/checkpoint-3000'
-            model = BartForConditionalGeneration.from_pretrained(checkpoint, config=config)  #, forced_bos_token_id=tokenizer.bos_token_id)
+            checkpoint = './test-bart-raw-slots/checkpoint-3000'
+            model = BartForConditionalGeneration.from_pretrained(checkpoint, config=config)
         elif run_args.model_type.lower() == 't5':
             checkpoint = 't5-small'
 
             # config = T5Config.from_pretrained(checkpoint)
             tokenizer = T5Tokenizer.from_pretrained(checkpoint)
-            # checkpoint = './test-t5/checkpoint-3000'
+            checkpoint = './test-t5/checkpoint-3000'
             model = T5ForConditionalGeneration.from_pretrained(checkpoint)
 
         train_dataset = SlotDataset(dataset=run_args.dataset, mode='train', tokenizer=tokenizer)
@@ -81,7 +82,7 @@ def main(run_args, model_config):
 
         # Use Huggingface's Trainer class for training and evaluation
         training_args = Seq2SeqTrainingArguments(
-            output_dir="test-t5",
+            output_dir="test-bart-with-template",
             # evaluation_strategy="epoch",
             dataloader_num_workers=4,
             per_device_train_batch_size=8,
@@ -101,10 +102,10 @@ def main(run_args, model_config):
             tokenizer=tokenizer
         )
 
-        trainer.train()
+        # trainer.train()
         # trainer.evaluate()
-
-        # predict_results = model.generate(test_dataset[0]['input_ids'].unsqueeze(0), decoder_start_token_id=tokenizer.eos_token_id)
+        # predict_results = model.generate(test_dataset[0]['input_ids'].unsqueeze(0),
+        #   decoder_start_token_id=tokenizer.eos_token_id)
         # predict_results = trainer.predict(test_dataset)
 
         # Select a subset of the test dataset for evaluation because the whole set does not fit into GPU memory
@@ -141,7 +142,14 @@ def main(run_args, model_config):
             # if list(intent_slot_mapping) != list(pred_slots.keys()):
             #     print(intent_slot_mapping, '\n', pred_slots.keys())
 
-            for slot_name, slot_pred in pred_slots.items():
+
+            intent = test_dataset.intents[i]
+            intent_slot_mapping = test_dataset.intent_slots_mapping[intent]
+            for slot_name, slot_pred in zip(intent_slot_mapping, pred_slots):
+
+            # for slot_name, slot_pred in pred_slots.items():
+
+                slot_pred = slot_pred.lower()
                 gold_slot = gold_slots.get(slot_name, '')
                 if slot_pred != 'none':
                     if gold_slot == '':
@@ -176,12 +184,16 @@ def main(run_args, model_config):
         #     if pred.split()[-1] != label:
         #         print(f"True label: {init_label: >15} \tMapped label: {label: >15}\tPrediction: {pred}")
 
-    elif run_args.approach.lower() == 'manual_prompting' and run_args.model_type.lower() == 'test-bart':
-        checkpoint = 't5-small'
-        #
+    elif run_args.approach.lower() == 'manual_prompting' and run_args.model_type.lower() == 'test-t5':
+        # checkpoint = 'facebook/bart-base'
         # config = BartConfig.from_pretrained(checkpoint)
         # config.force_bos_token_to_be_generated = True
+        # # config.forced_bos_token_id = tokenizer.bos_token_id
+        #
+        # tokenizer = BartTokenizer.from_pretrained(checkpoint)
+        # model = BartForConditionalGeneration.from_pretrained(checkpoint, config=config)
 
+        checkpoint = 't5-small'  # 't5-base'
         tokenizer = T5Tokenizer.from_pretrained(checkpoint)
         model = T5ForConditionalGeneration.from_pretrained(checkpoint)
 
@@ -189,25 +201,23 @@ def main(run_args, model_config):
         val_dataset = SlotDataset(dataset=run_args.dataset, mode='dev', tokenizer=tokenizer)
         test_dataset = SlotDataset(dataset=run_args.dataset, mode='test', tokenizer=tokenizer)
         train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=8, num_workers=4)
-        # val_dataloader = DataLoader(val_dataset, shuffle=False, batch_size=8, num_workers=4)
+        val_dataloader = DataLoader(val_dataset, shuffle=False, batch_size=8, num_workers=4)
         test_dataloader = DataLoader(test_dataset, shuffle=False, batch_size=8, num_workers=4)
 
-        train_seq2seq_model(model, tokenizer, train_dataloader, None, test_dataloader=test_dataloader)
-
+        train_seq2seq_model(model, tokenizer, train_dataloader, val_dataloader, test_dataloader, model_weights_path=None)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--dataset", default="snips", type=str, help="The input dataset")
     parser.add_argument("--approach", default="manual_prompting", type=str,
                         help="Select approach between `prompting` and `fine-tuning`")
     # parser.add_argument("--model_dir", default=None, required=True, type=str, help="Path to save, load model")
-    # parser.add_argument("--data_dir", default="./data", type=str, help="The input data directory")
-    parser.add_argument("--dataset", default="snips", type=str, help="The input dataset")
-    parser.add_argument("--model_type", default="test-bart", type=str, help="Select model type")
-    parser.add_argument('--seed', type=int, default=42, help="Seed for reproducibility")
+    parser.add_argument("--model_type", default="test-t5", type=str, help="Select model type")
+    parser.add_argument("--seed", type=int, default=42, help="Seed for reproducibility")
 
-    parser.add_argument('--do_train', default=True, type=bool, help="Whether to train the model.")
+    parser.add_argument("--do_train", default=True, type=bool, help="Whether to train the model.")
     parser.add_argument("--do_eval", default=True, type=bool, help="Whether to evaluate the model on the test set.")
 
     parser.add_argument("--save_preds", default=True, type=bool, help="Whether to save model's predictions to csv.")
